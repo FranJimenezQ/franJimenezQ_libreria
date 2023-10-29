5964827940cb062c0d6ce70531163d94924fdbcf
FranJimenezQ <flakundo@gmail.com>
2023-10-29 12:05:26 -0600

    sonarAnalysis updated 4

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -2,9 +2,8 @@ def call(def abortOnQualityGateFailure = false, def abortPipeline = false) {
     sh 'echo “Ejecución de las pruebas de calidad de código”'
     
     timeout(time: 5, unit: 'MINUTES') {
-        def qualityGateStatus = sh(script: 'curl -u [![Quality Gate Status]
-        (http://localhost:9000/api/project_badges/measure?project=DevOps_practica2&metric=alert_status&token=sqb_d093caf346a02a6bf951405197b5514836b0c15c)]
-        (http://localhost:9000/dashboard?id=DevOps_practica2): http://localhost:9000/api/qualitygates/project_status?projectKey=DevOps_practica2', 
+        def qualityGateStatus = sh(script: 'curl -u [![Quality Gate Status]sqb_d093caf346a02a6bf951405197b5514836b0c15c)]
+        http://localhost:9000/api/qualitygates/project_status?projectKey=DevOps_practica2', 
         returnStdout: true).trim()
         
         if (qualityGateStatus.contains('"status":"ERROR"')) {
